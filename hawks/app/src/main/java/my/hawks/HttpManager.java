f02d6f79fc08b2934a34ef80265874ceca416fb0
garunkumarcse <arunkumar.cse@gmail.com>
2015-03-14 11:33:29 -0500

    new methods  Locate friend

--- hawks/app/src/main/java/my/hawks/HttpManager.java
+++ hawks/app/src/main/java/my/hawks/HttpManager.java
@@ -5,12 +5,10 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -88,21 +86,22 @@ public class HttpManager {
         return entities;
     }
 
-    public static void setData(String uri, double lat, double lon, String email) {
+    public static void postData(MapEntity entity) {
         try {
             HttpClient client = new DefaultHttpClient();
-            HttpPost post = new HttpPost(uri);
+            HttpPost post = new HttpPost("");
             List<NameValuePair> pairs = new ArrayList<NameValuePair>();
 
 
             JSONObject jsonobj = new JSONObject();
-            jsonobj.put("UserName", email);
-            jsonobj.put("Latitude", lat);
-            jsonobj.put("Longitude", lon);
+            jsonobj.put("UserName", entity.getUserName());
+            jsonobj.put("UserId", entity.getUserId());
+            jsonobj.put("Latitude", entity.getLatitude());
+            jsonobj.put("Longitude", entity.getLongitude());
 
             pairs.add(new BasicNameValuePair("latLongData", jsonobj.toString()));
 
-            post.setEntity(new UrlEncodedFormEntity(pairs, "UTF-8"));
+            post.setEntity(new UrlEncodedFormEntity(pairs,"UTF-8"));
           /*  StringEntity se = new StringEntity(jsonobj.toString());
             post.setContentType("application/json;charset=UTF-8");
             se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json;charset=UTF-8"));*/
@@ -120,6 +119,85 @@ public class HttpManager {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    public static ArrayList<MapEntity> getAllMyFriends(String userId){
+        ArrayList<MapEntity> friendsList = null;
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost("");
+        List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+        JSONObject jsonobj = new JSONObject();
+        JSONArray jsonArray = null;
+        MapEntity entity = null;
+
+        try{
+
+            jsonobj.put("UserId", userId);
+            pairs.add(new BasicNameValuePair("latLongData", jsonobj.toString()));
+
+            post.setEntity(new UrlEncodedFormEntity(pairs, "UTF-8"));
+
+            HttpResponse httpresponse = client.execute(post);
+
+            String responseText = EntityUtils.toString(httpresponse.getEntity());
+
+            if(responseText != null){
+                jsonArray = new JSONArray(responseText);
+                friendsList = new ArrayList<MapEntity>();
+            }
+
+            for(int i=0; i<jsonArray.length(); i++){
+                entity = new MapEntity();
+                entity.setUserName(jsonArray.getJSONObject(i).getString("UserName"));
+                entity.setUserId(jsonArray.getJSONObject(i).getString("UserId"));
+                friendsList.add(entity);
+            }
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return friendsList;
+    }
+
+    public static MapEntity getMyFriendLocation(String friendUserId){
+        MapEntity friendEntity = null;
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost("");
+        List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+        JSONObject jsonobj = new JSONObject();
+        JSONArray jsonArray = null;
+        MapEntity entity = null;
+
+        try{
+
+            jsonobj.put("UserId", friendUserId);
+            pairs.add(new BasicNameValuePair("latLongData", jsonobj.toString()));
+
+            post.setEntity(new UrlEncodedFormEntity(pairs, "UTF-8"));
+
+            HttpResponse httpresponse = client.execute(post);
+
+            String responseText = EntityUtils.toString(httpresponse.getEntity());
+
+            if(responseText != null){
+                jsonArray = new JSONArray(responseText);
+                friendEntity = new MapEntity();
+                friendEntity.setUserName(jsonArray.getJSONObject(0).getString("UserName"));
+                friendEntity.setUserId(jsonArray.getJSONObject(0).getString("UserId"));
+                friendEntity.setUserName(jsonArray.getJSONObject(0).getString("Latitude"));
+                friendEntity.setUserId(jsonArray.getJSONObject(0).getString("Longitude"));
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return friendEntity;
     }
 
 }
